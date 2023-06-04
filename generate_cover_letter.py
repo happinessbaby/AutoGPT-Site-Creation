99cfe74577879bcb137eb8a9c1b61abb952139d3
happinessbaby <yueqipeng2021@gmail.com>
2023-06-04 00:15:22 -0500

    added langchain

--- generate_cover_letter.py
+++ generate_cover_letter.py
@@ -1,14 +1,17 @@
 # Import the necessary modules
 import os
 from openai_api import get_completion1, get_completion2
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 # Define the function to generate the cover letter
+chat = ChatOpenAI(temperature=0.0)
 
-def generate_cover_letter(my_name, resume_file, company_name):
+def generate_cover_letter(my_name, resume_file, my_company):
     # Read the resume file
     with open(resume_file, 'r') as f:
         resume = f.read()
-    print(resume)
+    # print(resume)
     
     # Extract the relevant information from the resume
     # name = resume.split('\n')[0]
@@ -19,13 +22,22 @@ def generate_cover_letter(my_name, resume_file, company_name):
     
     # Use an LLM to generate a cover letter that is specific to the resume file that is being read
     
-    template = f"Dear Hiring Manager, I am writing to express my interest in the {company_name} position at your company. My name is {my_name} ......"
+    # style_string = f"Dear Hiring Manager, I am writing to express my interest in the {company_name} position at your company. My name is {my_name} ......"
 
-    prompt = f"""You're to generate a cover letter for person with name {my_name} applying to company {company_name}. This is the template: {template}. This is the content based on which you're to generate a personalized cover letter: '''{resume}'''"""
+    template_string = """Generate a cover letter for person with name {name} applying to company {company}. 
+      The content you use to generate this personalized cover letter is delimited by triple backticks. content: '''{content}'''"""
 
-    # print(prompt)
+    prompt_template = ChatPromptTemplate.from_template(template_string)
 
-    cover_letter = get_completion2(prompt)
+    print(prompt_template.messages[0].prompt.input_variables)
+
+    cover_letter_message = prompt_template.format_messages(
+                    # style=cover_letter_template,
+                    name = my_name,
+                    company = my_company,
+                    content=resume)
+    cover_letter = chat(cover_letter_message).content
+    # cover_letter = get_completion2(template_string)
     
     
     # Write the cover letter to a file
